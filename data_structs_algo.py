50805f729ab879d334c759a0cf85b7a82409a4fc
lauroteles <laurotfl@gmail.com>
2024-12-19 13:58:12 -0300

    Big O

---
+++ data_structs_algo.py
@@ -0,0 +1,18 @@
+'''Big O structures'''
+
+''' O(n)'''
+def print_items(n):
+    for i in range(n):
+        print(i)
+
+
+''' O(n^2)'''
+
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            print(i,j)
+
+
+
+
