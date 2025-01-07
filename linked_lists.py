bb337aebad171da80c4bf3c74a3eddf64d6dc85d
lauroteles <laurotfl@gmail.com>
2025-01-07 14:15:41 -0300

    07 01

--- linked_lists.py
+++ linked_lists.py
@@ -90,25 +90,79 @@ class LinkedList:
             temp = temp.next
             return temp
 
+    '''LL Set'''
+    def set_value(self,index,value):
+            temp = self.get(index)
+            if temp:
+                temp.value = value
+                return True
+            return False
 
+    '''LL Insert'''
+    def insert(self, index , value):
+        if index < 0 or index >= self.length:
+            return False
+
+        if index == 0:
+            return self.prepend(value)
+
+        if index == self.length:
+            self.append(value) 
+
+        new_node = Node(value)
+        temp = self.get(index -1 )
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+    '''LL remove'''
+    def remove(self,index):
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            return self.pop_first()
+        if index == self.length -1:
+            return self.pop()
+
+        temp = self.get(index)
+        pre = self.get(index -1)
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+
+        return temp
 
 my_linked_list = LinkedList(5)        
 print(my_linked_list.tail.value)
 
 my_linked_list.print_list()
 
-my_linked_list.append(3)    
+my_linked_list.append(155)
+my_linked_list.append(310)
+my_linked_list.append(3233)    
 my_linked_list.append(5)    
 my_linked_list.append(7)    
 my_linked_list.append(15)    
-print(my_linked_list.tail.value)
+print('Value of the tail of the list -------> ',my_linked_list.tail.value)
 
 my_linked_list.prepend(10)
-print(my_linked_list.head.value)
+print('Added value to head of the lsit  -------> ',my_linked_list.head.value)
 
 my_linked_list.pop_first()
-print(f"Pop First {my_linked_list.tail.value}")
+print(f"Pop First ------> {my_linked_list.tail.value}")
 
 my_linked_list.get(2)
-print(f'Get {my_linked_list.get(1).value}')
-print(my_linked_list)
+print(f'Get ------> {my_linked_list.get(1).value}')
+
+my_linked_list.set_value(1, 100)
+print(f'Set ------> {my_linked_list.get(1).value}')
+
+my_linked_list.insert(2, 20)  
+print(f'Insert -----> {my_linked_list.get(2).value}')
+
+my_linked_list.remove(2)
+print(f'Remove -----> {my_linked_list.get(2).value}')
+
+
+print('----------   Length of the list   --------')
+my_linked_list.print_list()
