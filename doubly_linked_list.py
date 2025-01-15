9fb94f1f7a91587f2e24beba84e73c203c965993
lauroteles <laurotfl@gmail.com>
2025-01-15 14:26:31 -0300

    15 01

---
+++ doubly_linked_list.py
@@ -0,0 +1,69 @@
+
+class Node:
+    
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+        self.prev = None 
+
+
+class DoublyLinkedList:
+    
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self,value):
+
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node    
+        self.length += 1
+        return True
+
+    def pop(self):
+        temp = self.tail
+        if self.length == 0:
+            return None
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
+    
+    def prepend(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            new_node.prev = None
+            self.head.prev = new_node
+            self.head = new_node
+
+        self.length += 1
+        return True 
+
+my_double_linked_list = DoublyLinkedList(10)
+
+my_double_linked_list.print_list()
